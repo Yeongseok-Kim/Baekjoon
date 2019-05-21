aa6f14e45729d2ea6d224e79636f5033b567a073
Yeongseok-Kim <50166513+Yeongseok-Kim@users.noreply.github.com>
2019-05-22 00:37:21 +0900

    Update 1924.py
    변수형 오류 수정 및 논리 오류 수정

--- 1924.py
+++ 1924.py
@@ -1,9 +1,11 @@
 # 오늘은 2007년 1월 1일 월요일이다. 그렇다면 2007년 x월 y일은 무슨 요일일까?
 
 x,y=input().split()
+x=int(x)
+y=int(y)
 day=0
 
-for i in range(1,x+1):
+for i in range(0,x):
     if i in [1,3,5,7,8,10,12]:
         day+=31
     elif i in [4,6,9,11]:
