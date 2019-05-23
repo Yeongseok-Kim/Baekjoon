817a4b96b524f505b194614ed18f33a271bb7652
Yeongseok-Kim <50166513+Yeongseok-Kim@users.noreply.github.com>
2019-05-24 00:40:52 +0900

    Create 4344.py
    평균이 넘는 학생들의 퍼센테이지를 출력하는 문제

---
+++ 4344.py
@@ -0,0 +1,28 @@
+#평균이 넘는 학생들의 퍼센테이지를 출력하는 문제
+
+i=input()
+i=int(i)
+result=[]
+
+while i>0:
+    s=input().split()
+    s=list(map(float,s))
+    j=1
+    sumResult=0
+    sumResult=float(sumResult)
+    while s[0]>=j:
+        sumResult+=s[j]
+        j+=1
+    average=sumResult/s[0]
+    overAverage=0
+    j=1
+    while s[0]>=j:
+        if average<=s[j]:
+            overAverage+=1
+        j+=1
+    result.append(round((overAverage/s[0])*100,3))
+    i-=1
+
+for i in result:
+    print('%.3f'%i,end='')
+    print("%")
